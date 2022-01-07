159d5a0a7671d35f4e28dbea8ee466e31a0d7dce
lixiaopeng <lixiaopeng06@Macs-MacBook-Pro.local>
2022-01-07 14:46:53 +0800

    fixed

--- controller/study_module.controller.js
+++ controller/study_module.controller.js
@@ -1,13 +1,51 @@
 const mysql = require('../config/mysqlConfig')
 
 module.exports = {
-    getData: async (ctx, next) => {
+    get: async (ctx, next) => {
         const sql = `select * from study_module;`
         const query =  await mysql.mysqlQuery(sql)
         ctx.body = {
             code: 0,
             msg: '请求成功',
             data: query
+        }
+    },
+    post: async (ctx, next) => {
+        const param = ctx.request.body
+        const sql = `INSERT INTO study_module (name, src, tip) VALUES ('${param.name}', '${param.src}', '${param.tip}');`
+        const data = await mysql.mysqlQuery(sql)
+        ctx.body = {
+            code: 0,
+            msg: '添加成功',
+            data
+        }
+    },
+    delete: async (ctx, next) => {
+        const param = ctx.params
+        if (!param.id) {
+            ctx.body = {
+                code: 0,
+                msg: '删除失败',
+                data: false
+            }
+            return
+        }
+        const sql = `DELETE FROM study_module WHERE id=${param.id};`
+        await mysql.mysqlQuery(sql)
+        ctx.body = {
+            code: 0,
+            msg: '删除成功',
+            data: true
+        }
+    },
+    put: async (ctx, next) => {
+        const param = ctx.request.body
+        const sql = `UPDATE study_module SET name='${param.name}', src='${param.src}', tip='${param.tip}' WHERE id=${param.id};`
+        await mysql.mysqlQuery(sql)
+        ctx.body = {
+            code: 0,
+            msg: '更新成功',
+            data: true
         }
     }
 }
--- routes/study_module.routes.js
+++ routes/study_module.routes.js
@@ -1,6 +1,9 @@
 const router = require('koa-router')()
 const { study_module } = require('../controller')
 
-router.get('/', (ctx, next) => study_module.getData(ctx, next))
+router.get('/', (ctx, next) => study_module.get(ctx, next))
+    .post('/', (ctx, next) => study_module.post(ctx, next))
+    .delete('/:id', (ctx, next) => study_module.delete(ctx, next))
+    .put('/', (ctx, next) => study_module.put(ctx, next))
 
 module.exports = router;
