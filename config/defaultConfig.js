e844bf108724261f0638deced9af7ce07c719e20
lixiaopeng06 <lixiaopeng06.baidu.com>
2022-07-08 17:15:38 +0800

    mapauto6-15095

--- config/defaultConfig.js
+++ config/defaultConfig.js
@@ -2,7 +2,7 @@ const config = {
     user: 'root',
     password: 'lee13691388204',
     database: 'lee',
-    host: '127.0.0.1',
+    host: process.env.NODE_ENV === 'development' ? '127.0.0.1' : '120.48.89.210',
     port: 3306
 };
 
