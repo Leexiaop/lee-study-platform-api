50cf321e537085b4ed5a6ae19825b1f3fa331f00
lixiaopeng06 <lixiaopeng06.baidu.com>
2022-07-12 15:09:00 +0800

    mapauto6-15095 恢复默认

--- config/defaultConfig.js
+++ config/defaultConfig.js
@@ -2,7 +2,8 @@ const config = {
     user: 'root',
     password: 'lee13691388204',
     database: 'lee',
-    host: process.env.NODE_ENV === 'development' ? '127.0.0.1' : '120.48.89.210',
+    // host: process.env.NODE_ENV === 'development' ? '127.0.0.1' : '120.48.89.210',
+    host: '120.48.89.210',
     port: 3306
 };
 
